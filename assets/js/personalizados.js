ecdb4bca7efb654f01fd161f108218c374fe97cd
iespinoza98 <israelespinoza1298@gmail.com>
2022-06-07 23:36:51 -0500

    Imagen

--- assets/js/personalizados.js
+++ assets/js/personalizados.js
@@ -13,28 +13,31 @@ function addElements (sName) {
             descripcion: "Las empresas de hoy necesitan aprovechar al máximo Internet. Por lo tanto, tienen que desarrollar aplicaciones o sitios web donde puedan entregar sus productos o automatizar procesos comerciales.",
             caracteristicas: ["Buena usabilidad y organización", " Contenido de calidad", "Responsiva", "Diseño adaptado al estilo corporativo"],
             subtitulo: "La importancia de contar con una página web",
-            parrafo: "Cuando una organización tiene un sitio web, aumenta la reputación de la organización y permite que los clientes aumenten su  confianza en el producto o servicio que ofrece la organización. Los sitios web ayudan a aumentar las ventas, la productividad y el valor de mercado de cualquier PYME"
+            parrafo: "Cuando una organización tiene un sitio web, aumenta la reputación de la organización y permite que los clientes aumenten su  confianza en el producto o servicio que ofrece la organización. Los sitios web ayudan a aumentar las ventas, la productividad y el valor de mercado de cualquier PYME",
+            imagen: "assets/images/services/service-details.jpg"
         },
         "sTienda": {
             titulo: "Tiendas Online",
             descripcion: "Actualmente es muy importante tener una tienda online por lo que nos encargamos de ofrecerle una interfaz comercial para facilitar las ventas de tus productos.",
             caracteristicas: ["Diseño Responsive", "Pasarelas de Pago", "Análisis y Reportes", "Complementos Especializados"],
             subtitulo: "Síntesis para el diseño de tiendas",
-            parrafo: "En la actulidad existen diversas platformas(CMS) que permiten el diseño de tiendas virtuales con mayor rapidez. Para ello adaptamos un diseño exclusivo y estructura integrando todas las funcionalidades y plugins necesarios"
+            parrafo: "En la actualidad existen diversas platformas(CMS) que permiten el diseño de tiendas virtuales con mayor rapidez. Para ello adaptamos un diseño exclusivo y estructura integrando todas las funcionalidades y plugins necesarios",
+            imagen: "assets/images/services/service-details.jpg"
         },
         "sSistemas": {
             titulo: "Sistemas Web",
             descripcion: "Un sistema web personalizado le ahorrará costes, tiempo y recursos humanos. Como tal, las ganancias de su negocio aumentarán y administrará mejor a sus clientes, proveedores, distribuidores y más.",
             caracteristicas: ["Ahorran costes de hardware y software.", "Fáciles de usar", "Facilitan el trabajo colaborativo y a distancia", "Escalables y de rápida actualización"],
             subtitulo: "Será más eficiente; podrá ganar más y gastará menos.",
-            parrafo: "Utilice aplicaciones web para ahorrar dinero. Aprovechará mejor su tiempo porque no tiene que preocuparse por aprender nuevos programas, mantenerlos o hacer copias de seguridad de sus datos, y puede trabajar desde cualquier lugar."
+            parrafo: "Utilice aplicaciones web para ahorrar dinero. Aprovechará mejor su tiempo porque no tiene que preocuparse por aprender nuevos programas, mantenerlos o hacer copias de seguridad de sus datos, y puede trabajar desde cualquier lugar.",
+            imagen: "assets/images/services/service-details.jpg"
         }
     };
 
     let oServicio = aServicios[sName];
     
     $('#contentsService').empty();
-    $('#contentsService').append('<div class="image mb-40"> <img src="assets/images/services/service-details.jpg" alt="Service Details"> </div>');
+    $('#contentsService').append('<div class="image mb-40"> <img src="' + oServicio.imagen + '"> </div>');
     $('#contentsService').append('<h2>' + oServicio.titulo + '</h2>');
     $('#contentsService').append('<p>' + oServicio.descripcion + '</p>');
     if (oServicio.caracteristicas.length > 0) {
